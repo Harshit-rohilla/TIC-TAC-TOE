1b1746b69e4ba3fff6f4ef044e54e0bbf7728e14
Harshit Rohilla <harshitrohilla105@gmail.com>
2024-12-27 11:47:48 +0530

    added result section and changed the ui

--- script.js
+++ script.js
@@ -8,22 +8,26 @@ let box7=document.querySelector('.box-7')
 let box8=document.querySelector('.box-8')
 let box9=document.querySelector('.box-9')
 const btn=document.querySelector('.btn')
+const result=document.querySelector('.result')
 let chance ='X'
 let allBoxes=document.querySelectorAll('.boxes')
 
 btn.addEventListener('click',()=>{
     chance='X'
     allBoxes.forEach((box)=>{box.innerHTML=''})
+    result.innerHTML=''
 })
 
 allBoxes.forEach((box)=>{box.addEventListener('click',(e)=>{
     if(chance==='X'&& e.target.innerHTML===''){
         e.target.innerHTML='X';
-        chance='0'
+        e.target.style.color='rgb(84, 84, 84)';
+        chance='O'
         checkWinner();
     }
-    else if(chance==='0'&& e.target.innerHTML===''){
-        e.target.innerHTML='0'
+    else if(chance==='O'&& e.target.innerHTML===''){
+        e.target.innerHTML='O'
+        e.target.style.color='rgb(242, 235, 211)';
         chance='X'
         checkWinner();
     }
@@ -31,10 +35,10 @@ allBoxes.forEach((box)=>{box.addEventListener('click',(e)=>{
 
 function checkWinner(){
     if((box1.innerHTML==='X' && box2.innerHTML==='X' && box3.innerHTML==='X') || (box1.innerHTML==='X' && box4.innerHTML==='X' && box7.innerHTML==='X') || (box3.innerHTML==='X' && box6.innerHTML==='X' && box9.innerHTML==='X') || (box7.innerHTML==='X' && box8.innerHTML==='X' && box9.innerHTML==='X') || (box1.innerHTML==='X' && box5.innerHTML==='X' && box9.innerHTML==='X') || (box7.innerHTML==='X' && box5.innerHTML==='X' && box3.innerHTML==='X')){
-        alert('X WON')
+        result.innerHTML='X WON'
     }
-    else if((box1.innerHTML==='0' && box2.innerHTML==='0' && box3.innerHTML==='0') || (box1.innerHTML==='0' && box4.innerHTML==='0' && box7.innerHTML==='0') || (box3.innerHTML==='0' && box6.innerHTML==='0' && box9.innerHTML==='0') || (box7.innerHTML==='0' && box8.innerHTML==='0' && box9.innerHTML==='0') || (box1.innerHTML==='0' && box5.innerHTML==='0' && box9.innerHTML==='0') || (box7.innerHTML==='0' && box5.innerHTML==='0' && box3.innerHTML==='0')){
-        alert('0 WON')
+    else if((box1.innerHTML==='O' && box2.innerHTML==='O' && box3.innerHTML==='O') || (box1.innerHTML==='O' && box4.innerHTML==='O' && box7.innerHTML==='O') || (box3.innerHTML==='O' && box6.innerHTML==='O' && box9.innerHTML==='O') || (box7.innerHTML==='O' && box8.innerHTML==='O' && box9.innerHTML==='O') || (box1.innerHTML==='O' && box5.innerHTML==='O' && box9.innerHTML==='O') || (box7.innerHTML==='O' && box5.innerHTML==='O' && box3.innerHTML==='O')){
+        result.innerHTML='O WON'
     }
 }
 
